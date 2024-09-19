c287821137b20b8e6557d163d6dd78427f5e4e0b
Mukesh Rauniyar <mukeshrauniyar.me@gmail.com>
2024-09-19 16:33:59 +0545

    initial commit

--- app/page.js
+++ app/page.js
@@ -1,113 +1,136 @@
 import Image from "next/image";
 
+export const metadata = {
+  // title: "Mukesh's Dev Portfolio",
+  description: "hey this is Mukesh's Dev Portfolio",
+}
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+   <>
+       <header>
+      <nav>
+        <div className="left">Mukesh's Portfolio</div>
+        <div className="right">
+          <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/">About</a></li>
+            <li><a href="/">Services</a></li>
+            <li><a href="/">Projects</a></li>
+            <li><a href="/">Contact Me</a></li>
+          </ul>
         </div>
-      </div>
+      </nav>
+    </header>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    <main>
+      <section className="firstSection">
+        <div className="leftSection">
+          <div>Hey, My name is <span className="purple">Mukesh</span></div>
+          <div>and I am a passionate</div>
+          <span id="element"></span>
+          <div className="buttons">
+            <button className="btn">Download Resume</button>
+            <button className="btn">Visit Github</button>
+          </div>
+        </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        <div className="rightSection">
+          <img src="/bg.png" alt="bg" />
+        </div>
+      </section>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+      <hr/>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+       <section className="secondSection">
+        <span className="text-gray">What I have done so far</span>
+        <h1>Work Exprences</h1>
+        <div className="box">
+            <div className="vertical">
+                <img className="image-top" src="/htmllogo.png" alt=""/>
+                <div className="vertical-title">
+                    HTML Developer (2010-2012)
+                </div>
+                <div className="vertical-desc">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt, porro rerum. Nisi id corrupti, exercitationem fugiat cum atque explicabo placeat omnis architecto quis.
+                </div>
+            </div>
+            <div className="vertical">
+                <img className="image-top" src="/csslogo.png" alt=""/>
+                <div className="vertical-title">
+                    HTML Developer (2010-2012)
+                </div>
+                <div className="vertical-desc">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt, porro rerum. Nisi id corrupti, exercitationem fugiat cum atque explicabo placeat omnis architecto quis.
+                </div>
+            </div>
+            <div className="vertical">
+                <img className="image-top" src="/js.png" alt=""/>
+                <div className="vertical-title">
+                    HTML Developer (2010-2012)
+                </div>
+                <div className="vertical-desc">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt, porro rerum. Nisi id corrupti, exercitationem fugiat cum atque explicabo placeat omnis architecto quis.
+                </div>
+            </div>
+            <div className="vertical">
+                <img className="image-top" src="/nodejslogo.png" alt=""/>
+                <div className="vertical-title">
+                    HTML Developer (2010-2012)
+                </div>
+                <div className="vertical-desc">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt, porro rerum. Nisi id corrupti, exercitationem fugiat cum atque explicabo placeat omnis architecto quis.
+                </div>
+            </div>
+            <div className="vertical">
+                <img className="image-top" src="/react.png" alt=""/>
+                <div className="vertical-title">
+                    HTML Developer (2010-2012)
+                </div>
+                <div className="vertical-desc">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt, porro rerum. Nisi id corrupti, exercitationem fugiat cum atque explicabo placeat omnis architecto quis.
+                </div>
+            </div>
+        </div>
+       </section>
     </main>
+    
+    <footer>
+        <div className="footer">
+            <div className="footer-first">
+                <h3>Mukesh's Developer Portfolio</h3>
+            </div>
+            <div className="footer-second">
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Services</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+            <div className="footer-third">
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Services</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+            <div className="footer-forth">
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Services</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+        </div>
+        <div className="footer-rights">
+           Copyright &copy; mukeshsportfolio.com | All rights reserved
+        </div>
+    </footer>
+
+    
+   </>
   );
 }
